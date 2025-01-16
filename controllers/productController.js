d0411e2aeaacba8d67c71bb299b7ed3ffe545d34
tungarazaa <tungarazakennedy@gmail.com>
2025-01-17 01:49:43 +0300

    Creating a Product features file

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 import Product from "../models/productModel.js";
+import ProductFeatures from "../utils/productFeatures.js";
 
 const topProducts = (req, res, next) => {
   req.query.sort = "-rating price";
@@ -9,42 +10,49 @@ const topProducts = (req, res, next) => {
 const getProducts = async (req, res) => {
   try {
     //FILTERING
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
+    // const queryObj = { ...req.query };
+    // const excludedFields = ["page", "sort", "limit", "fields"];
+    // excludedFields.forEach((el) => delete queryObj[el]);
 
     //ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    let query = Product.find(JSON.parse(queryStr));
+    // let queryStr = JSON.stringify(queryObj);
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // let query = Product.find(JSON.parse(queryStr));
+    // console.log(query);
 
     //SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(",").join(" ");
+    //   query = query.sort(sortBy);
+    // } else {
+    //   query = query.sort("-createdAt");
+    // }
 
     //FIELDS LIMITING
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(",").join(" ");
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select("-__v");
+    // }
 
     //PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const numProducts = await Product.countDocuments();
-      if (skip >= numProducts) throw new Error("No result found!");
-    }
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
+    // query = query.skip(skip).limit(limit);
+    // if (req.query.page) {
+    //   const numProducts = await Product.countDocuments();
+    //   if (skip >= numProducts) throw new Error("No result found!");
+    // }
 
-    const products = await query;
+    const features = new ProductFeatures(Product.find(), req.query)
+      .filter()
+      .limitFields()
+      .sort()
+      .paginate();
+
+    const products = await features.query;
     //REPONSE
     res.status(200).json({
       status: "success",
